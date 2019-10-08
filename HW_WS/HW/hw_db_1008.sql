ebfd15c1dbc8c8f838813505f9cca137eaa58c74
조민 <minnaldo6602@gmail.com>
2019-10-08 18:03:18 +0900

    hw_1008

--- HW_WS/HW/hw_db_1008.sql
+++ HW_WS/HW/hw_db_1008.sql
@@ -4,23 +4,29 @@ use wsdb03;
 -- 1. 부서위치가 CHICAGO인 모든 사원에 대해 이름,업무,급여 출력하는 SQL을 작성하세요.
 select * from dept where loc = 'CHICAGO';
 
-select ename, job, sal
-from dept inner join emp
-where loc = 'CHICAGO';
+select a.ename, a.job, a.sal 
+from emp a inner join dept b on a.deptno=b.deptno
+where b.loc='chicago';
 
 select * from dept;
 select * from emp;
 
--- 2. 부하직원이 없는 사원의 사원번호,이름,업무,부서번호 출력하는 SQL을 작성하세요.
+-- 2) 부하직원이 없는 사원의 사원번호, 이름, 업무, 부서번호 출력하는 sql
+select empno, ename, job, deptno 
+from emp 
+where empno not in (select distinct mgr from emp where mgr is not null);
 
+-- 3) Blake와 같은 상사를 가진 사원의 이름,업무, 상사번호 출력하는 sql
+select ename, job, mgr 
+from emp 
+where mgr in (select mgr from emp where mgr = 
+               (select empno from emp where ename='blake') );
 
+-- 4) 입사일이 가장 오래된 사람 5명 검색
+select * from emp order by HIREDATE asc limit 5;
 
--- 3. BLAKE와 같은 상사를 가진 사원의 이름,업무,상사번호 출력하는 SQL을 작성하세요.
-
-
--- 4. 입사일이 가장 오래된 사람 5명을 검색하세요.
-select *
-from emp
-where hiredate 
-
--- 5. JONES 의 부하 직원의 이름, 업무, 부서명을 검색하세요.
+-- 5) jones의 부하직원의 이름, 업무, 부서명을 검색
+select e.ename, e.job, d.dname 
+from emp e join dept d on e.deptno=d.deptno 
+where e.mgr in (select mgr from emp where mgr = 
+                 (select empno from emp where ename='jones'));
